5668e2a55d3db2d52a797fadb6b5fe096db3a2c4
sunset535 <shenxiaolong535@gmail.com>
2015-01-22 16:48:37 +0800

    add file

---
+++ src/com/zxdp/adapter/MainFrame.java
@@ -0,0 +1,50 @@
+package com.zxdp.adapter;
+
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.Window;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
+public class MainFrame extends JFrame{
+
+	private static final long serialVersionUID = 1L;
+
+	private static JButton selectOriginFileBtn;
+	private static JTable table;
+	private static JButton selectGoalFilePathBtn;
+	private static JTextField goalFilePath;
+	
+	public MainFrame(){
+		init();
+	}
+	
+	public void init(){
+		selectOriginFileBtn = new JButton("选择文件");
+		table = new JTable();
+		selectGoalFilePathBtn = new JButton("选择目录");
+		goalFilePath = new JTextField();
+		
+		JFrame frame = new JFrame("点评宝-格式转换器");
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(selectOriginFileBtn, BorderLayout.CENTER);
+		frame.setIconImage(new ImageIcon("Source/image/logo.png").getImage());
+	    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();  
+		frame.setLocation(screenSize.width/2-350, screenSize.height/2-250);
+		frame.setLayout(null);
+		frame.pack();
+		frame.setSize(700, 500);
+		frame.setVisible(true);
+	}
+	
+	
+}
--- src/com/zxdp/main/Entrance.java
+++ src/com/zxdp/main/Entrance.java
@@ -1,8 +1,12 @@
 package com.zxdp.main;
 
+import com.zxdp.adapter.MyFileDialog;
+import com.zxdp.adapter.MainFrame;
+
 public class Entrance {
 
 	public static void main(String[] args){
-		
+		new MainFrame();
+		//new MyFileDialog();
 	}
 }
